eeb4bcb9297b58260cd0b7dc77eac8adf90f4c24
Lucas <fernandes.mateus@escolar.ifrn.edu.br>
2021-01-08 09:09:50 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsliver/DsliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsliver/DsliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
